b8bef9535ce04047d0633b47c9fccb10f92c6b33
arthurs123 <108904420+arthurs123@users.noreply.github.com>
2026-04-16 15:52:02 -0300

    Arrays

---
+++ src/Aula1.ts
@@ -0,0 +1,13 @@
+let vetor:Array<string|number>=["José", 21,"mateus"]
+
+vetor.push(30) //adicionar elemento no final
+vetor.pop() //excluir o ultimo elemento
+vetor.unshift("roxo") //adiciona um elemento no inicio
+vetor.shift() //remove o primeiro elemento
+vetor.reverse() //inverte o vetor
+vetor.sort() //ordena em ordem alfabética e crescente
+
+
+
+
+console.log(vetor)
