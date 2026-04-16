d91c0753890f0ad57ca9edaf6299d38fc7531617
arthurs123 <108904420+arthurs123@users.noreply.github.com>
2026-04-16 14:25:16 -0300

    novos arquivos
    arquivos de aula

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// function saudaçao(nome:string):string{
+//     return `Olá, ${nome}`;
+// }
+// console.log(saudaçao('mundo'));
+
+
+
+let valor:number=Number(prompt("Digite um valor: "));
+
+if (valor>0){
+    alert("positivo");
+}
+else if(valor<0){
+    alert("negativo");
+}
+else{
+    alert("nulo");
+}
