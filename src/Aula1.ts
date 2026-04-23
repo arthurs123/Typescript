95ab4cb388f6713cf9ec9c278af39533af5b5d1e
arthurs123 <108904420+arthurs123@users.noreply.github.com>
2026-04-23 15:52:03 -0300

    1º questão da lista

--- src/Aula1.ts
+++ src/Aula1.ts
@@ -1,13 +1,15 @@
-let vetor:Array<string|number>=["José", 21,"mateus"]
+// let vetor:Array<string|number>=["José", 21,"mateus"]
 
-vetor.push(30) //adicionar elemento no final
-vetor.pop() //excluir o ultimo elemento
-vetor.unshift("roxo") //adiciona um elemento no inicio
-vetor.shift() //remove o primeiro elemento
-vetor.reverse() //inverte o vetor
-vetor.sort() //ordena em ordem alfabética e crescente
+// vetor.push(30) //adicionar elemento no final
+// vetor.pop() //excluir o ultimo elemento
+// vetor.unshift("roxo") //adiciona um elemento no inicio
+// vetor.shift() //remove o primeiro elemento
+// vetor.reverse() //inverte o vetor
+// vetor.sort() //ordena em ordem alfabética e crescente
+// vetor.length //conta a quantidade de elementos
+// vetor.join()
+ 
 
 
+// console.log(vetor)
 
-
-console.log(vetor)
---
+++ src/lista/arrays/Q1.ts
@@ -0,0 +1,20 @@
+// 1. Mostre-me as seguintes listas, derivadas de: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+// a) Números pares
+// b) Números ímpares
+// c) Todos os múltiplos de 2,3 e 4
+// d) Lista reversa
+
+
+let i;
+let pares=[]
+let impares=[]
+let mult=[]
+
+const vetor = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+for(i=0; i<length-1;i++){
+    if (vetor[i] %2==0){
+        pares[i]=vetor[i]
+    }
+    
+};
