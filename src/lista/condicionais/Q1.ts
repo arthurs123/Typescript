d3cb6d911ee392da95bac6a9627720999f078730
arthurs123 <108904420+arthurs123@users.noreply.github.com>
2026-04-26 00:24:02 -0300

    mudanças

---
+++ src/lista/condicionais/Q1.ts
@@ -0,0 +1,14 @@
+// 1. Crie um programa que leia um número e informe se ele é:
+//  Par ou Ímpar
+//  Positivo ou Negativo
+
+
+
+let num:number=Number(prompt("Informe um numero: "))
+
+if (num %2==0){
+    console.log("Positivo")
+}
+else if(num%2!=0){
+    console.log("negativo")
+}
